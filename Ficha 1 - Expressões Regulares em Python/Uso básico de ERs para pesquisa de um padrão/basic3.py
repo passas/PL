0c81414c8076d2b36886539a3b819d7fe033e2cb
Sérgio Costa <48985218+passas@users.noreply.github.com>
2024-02-14 17:08:29 +0000

    Exercício 4

---
+++ Ficha 1 - Expressões Regulares em Python/Uso básico de ERs para pesquisa de um padrão/basic3.py
@@ -0,0 +1,32 @@
+import re
+
+
+#Definição que comporta a expressão regular -raw string- acerca da forma como queremos apanhar/capturar o conteúdo de uma linha
+expressao_regular = r'[Hh][Ee][Ll]{2}[Oo]'
+#expressao_regular = r'(?i:hello)'
+
+#Definição que comporta a palavra -string- substituta
+string_substituta = '*YEP*'
+
+
+#Main
+if __name__ == '__main__':
+
+  #Lêr uma linha -stdin- e guardá-la num buffer -string-
+  buffer_stdin = input(">> ")
+  
+  #Parar o ciclo se a linha fôr vazia
+  while buffer_stdin != "":
+
+    #Procurar, e substituir, todas as ocorrências, na linha, relativas à expressão regular e guardar o resultado numa variável -string-
+    string_resultado = re.sub(expressao_regular, string_substituta, buffer_stdin)
+    
+    #Encontrei, e substituí, a expressão regular na linha
+    if(string_resultado != buffer_stdin):
+      
+      #-stdout-: Resultado -string- da substituição das expressões encontradas
+      print (string_resultado)
+
+
+    #Lêr uma linha -stdin- e guardá-la num buffer -string-
+    buffer_stdin = input(">> ")
